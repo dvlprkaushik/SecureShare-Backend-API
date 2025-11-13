7b885fdf61797a3c5dc3b74dbe1e8521aeda024b
kaushik <dvlprkaushik@gmail.com>
2025-11-14 00:18:45 +0530

    feat: implement cloudinary service for upload and delete operations | streamifier dependency installation

---
+++ src/services/cloudinary.services.ts
@@ -0,0 +1,50 @@
+import cloudinary from "@/config/cloudinary.config.js";
+import streamifier from "streamifier";
+import type { UploadApiResponse } from "cloudinary";
+import { StorageError } from "@/utils/StorageError.js";
+
+export const uploadToCloudinary = (
+  file: Express.Multer.File
+): Promise<UploadApiResponse> => {
+  return new Promise((resolve, reject) => {
+    if (!file?.buffer) {
+      return reject(
+        new Error("File buffer not provided")
+      );
+    }
+
+    const uploadStream = cloudinary.uploader.upload_stream(
+      { resource_type: "auto" },
+      (error: any, result: UploadApiResponse | undefined) => {
+        if (error) return reject(error);
+
+        if (!result) {
+          return reject(
+            new StorageError("UPLOAD_FAILED", "Cloudinary returned no result")
+          );
+        }
+
+        resolve(result);
+      }
+    );
+
+    streamifier
+      .createReadStream(file.buffer)
+      .on("error", reject)
+      .pipe(uploadStream);
+  });
+};
+
+export const deleteFromCloudinary = async (
+  publicId: string,
+  options?: { resource_type?: "image" | "raw" | "video" }
+) => {
+  try {
+    return await cloudinary.uploader.destroy(publicId, options);
+  } catch (err) {
+    throw new StorageError(
+      "DELETE_FAILED",
+      "Failed to delete file from Cloudinary"
+    );
+  }
+};
