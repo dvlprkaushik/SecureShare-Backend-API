e3eaee11e867bb8fd0f56c93a8e8ee2f9cb4d848
kaushik <dvlprkaushik@gmail.com>
2025-11-17 11:34:12 +0530

    feat: add share routes for generating, accessing and revoking share links

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { authRouter } from "./v1/auth.routes.js";
 import { fileRouter } from "./v1/files.routes.js";
 import { folderRouter } from "./v1/folder.routes.js";
+import { shareRouter } from "./v1/share.routes.js";
 
 const v1Routes = Router();
 
@@ -13,5 +14,8 @@ v1Routes.use("/files", fileRouter);
 
 // folder routes
 v1Routes.use("/folders", folderRouter);
+
+// share routes
+v1Routes.use("/share", shareRouter);
 
 export { v1Routes };
