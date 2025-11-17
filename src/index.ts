efcf04fb7617e747e44fa03ec43629b0a4d7717b
kaushik <dvlprkaushik@gmail.com>
2025-11-17 14:49:02 +0530

    chore: import and register not-found and error handler middlewares in index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import { listener } from "./listener.js";
 import { healthRoutes } from "./healthCheck.js";
 import { serverConfig as scf } from "./config/env.config.js";
 import { v1Routes } from "./routes/index.routes.js";
+import { notFound } from "./middleware/notFound.middleware.js";
+import { errorhandler } from "./middleware/errorHandler.middleware.js";
 
 dotenv.config({ quiet: true });
 
@@ -20,6 +22,14 @@ app.use("/api/v1",v1Routes);
 
 // Health routes
 healthRoutes(app);
+
+
+// !error handling routes
+// not found handler
+app.use(notFound);
+
+// global error handler
+app.use(errorhandler);
 
 // Server Listen
 listener(app);
