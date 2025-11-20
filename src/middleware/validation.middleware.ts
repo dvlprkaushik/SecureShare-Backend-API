02f8878ebd97a4c2721ccef1bc741641c216b5c6
kaushik <dvlprkaushik@gmail.com>
2025-11-20 20:09:32 +0530

    refactor: formatting validation middleware

--- src/middleware/validation.middleware.ts
+++ src/middleware/validation.middleware.ts
@@ -1,27 +1,35 @@
 import { Request, Response, NextFunction } from "express";
-import {z} from "zod";
+import { z } from "zod";
 
-type ValidationSource = 'body' | 'params' | 'query';
+type ValidationSource = "body" | "params" | "query";
 
 export const validate = <T extends z.ZodType>(
-  schema : T,
-  source : ValidationSource = 'body'
+  schema: T,
+  source: ValidationSource = "body"
 ) => {
-  return (req : Request, _res : Response, next : NextFunction) =>{
-      const receivedData = source === "body" ? req.body : source === "params" ? req.params : req.query;
+  return (req: Request, _res: Response, next: NextFunction) => {
+    const receivedData =
+      source === "body"
+        ? req.body
+        : source === "params"
+        ? req.params
+        : req.query;
 
-      const validatedData = schema.safeParse(receivedData);
+    const validatedData = schema.safeParse(receivedData);
 
-      if(!validatedData.success){
-        return next(validatedData.error);
-      }
+    if (!validatedData.success) {
+      return next(validatedData.error);
+    }
 
     (req as any)[source] = validatedData.data;
 
-      next()
-  }
-}
+    next();
+  };
+};
 
-export const validateBody = <T extends z.ZodType>(schema: T) => validate(schema, 'body');
-export const validateParams = <T extends z.ZodType>(schema: T) => validate(schema, 'params');
-export const validateQuery = <T extends z.ZodType>(schema: T) => validate(schema, 'query');
+export const validateBody = <T extends z.ZodType>(schema: T) =>
+  validate(schema, "body");
+export const validateParams = <T extends z.ZodType>(schema: T) =>
+  validate(schema, "params");
+export const validateQuery = <T extends z.ZodType>(schema: T) =>
+  validate(schema, "query");
