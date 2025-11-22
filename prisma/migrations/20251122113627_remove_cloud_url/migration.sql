dd403630d394e3051cbbb2e46639574908860051
kaushik <dvlprkaushik@gmail.com>
2025-11-22 18:00:51 +0530

    fix(prisma): removed cloudUrl field from FileMetaData for private delivery setup

---
+++ prisma/migrations/20251122113627_remove_cloud_url/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `cloudUrl` on the `FileMetaData` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "FileMetaData" DROP COLUMN "cloudUrl";
