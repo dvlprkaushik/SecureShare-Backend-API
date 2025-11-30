eb70154b332466ca675238b05875b9d6645e1c96
kaushik <dvlprkaushik@gmail.com>
2025-11-30 20:34:31 +0530

    refactor: added avatar module in postman collection and added new routes in file module, and more clear imports in avatar routes

--- src/routes/v1/avatar.routes.ts
+++ src/routes/v1/avatar.routes.ts
@@ -2,11 +2,7 @@ import { Router } from "express";
 import { authMiddleware } from "@/middleware/auth.middleware.js";
 import { validateBody } from "@/middleware/validation.middleware.js";
 
-import {
-  requestAvatarUploadUrl,
-  saveAvatar,
-  getAvatar
-} from "@/controllers/userAvatar.controller.js";
+import * as avatar_controller from "@/controllers/userAvatar.controller.js";
 
 import {
   avatarUploadSchema,
@@ -21,20 +17,20 @@ avatarRouter.post(
   avatarUploadLimiter,
   authMiddleware,
   validateBody(avatarUploadSchema),
-  requestAvatarUploadUrl
+  avatar_controller.requestAvatarUploadUrl
 );
 
 avatarRouter.post(
   "/complete",
   authMiddleware,
   validateBody(avatarCompleteSchema),
-  saveAvatar
+  avatar_controller.saveAvatar
 );
 
 avatarRouter.get(
   "/",
   authMiddleware,
-  getAvatar
+  avatar_controller.getAvatar
 );
 
 export { avatarRouter };
